5324316ea7a917f37f2223909457f53d1f4d79e5
bhattadeb34 <dxb5775@psu.edu>
2026-02-16 21:41:10 -0500

    Configure matplotlib for Colab

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 OUT_DIR = os.path.join(BASE_DIR, 'figures')
 
+os.makedirs(OUT_DIR, exist_ok=True)
+
 NIST_NEU_PATH = os.path.join(DATA_DIR, 'FINAL_PM7_ALL_neutral_cleaned.csv')
 NIST_PROT_PATH = os.path.join(DATA_DIR, 'FINAL_PM7_ALL_protonated_cleaned.csv')
 NIST_EXP_PATH = os.path.join(DATA_DIR, 'Dataset.csv')
@@ -22,3 +24,10 @@ AXES_TITLE_SIZE = 12
 LEGEND_FONT_SIZE = 9
 XTICK_LABEL_SIZE = 9
 YTICK_LABEL_SIZE = 9
+
+import matplotlib
+try:
+    if os.environ.get('COLAB_GPU') or os.environ.get('TRAMPOLINE_CI'):
+         matplotlib.use('Agg')
+except:
+    pass
